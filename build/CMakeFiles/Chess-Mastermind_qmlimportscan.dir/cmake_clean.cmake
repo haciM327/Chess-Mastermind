67353a8e6319467044b83e9586b8ed59837f67e7
haciM327 <micahdschwab@gmail.com>
2024-07-31 18:56:03 -0400

    removed build folder

--- build/CMakeFiles/Chess-Mastermind_qmlimportscan.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  ".qt/qml_imports/Chess-Mastermind_build.cmake"
-  "CMakeFiles/Chess-Mastermind_qmlimportscan"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Chess-Mastermind_qmlimportscan.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
